c6eb95cc24d6bda45c3789cb49da4b3bf0f025a1
mohammed-sherif34 <mohammedshereef342002@gmail.com>
2024-07-17 02:46:17 +0300

    finish radio tab ui

--- lib/home/radio/radio_tab.dart
+++ lib/home/radio/radio_tab.dart
@@ -5,8 +5,38 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('assets/images/radio_image.png'),
+        Text(
+          'إذاعة القرآن الكريم',
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ImageIcon(
+              color: Theme.of(context).primaryColor,
+              AssetImage(
+                'assets/images/back_radio_icon.png',
+              ),
+            ),
+            ImageIcon(
+              color: Theme.of(context).primaryColor,
+              AssetImage(
+                'assets/images/play_radio_icon.png',
+              ),
+            ),
+            ImageIcon(
+              color: Theme.of(context).primaryColor,
+              AssetImage(
+                'assets/images/next_radio_icon.png',
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
