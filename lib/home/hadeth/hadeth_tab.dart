68e51fb9314a3b9c1f789973b0f9ef49e5211211
mohammed-sherif34 <mohammedshereef342002@gmail.com>
2024-07-17 03:04:49 +0300

    finish hadeth ui tab

--- lib/home/hadeth/hadeth_tab.dart
+++ lib/home/hadeth/hadeth_tab.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/utils/app_colors.dart';
 
 class HadethTab extends StatelessWidget {
   const HadethTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Column(
+      children: [
+        Image.asset('assets/images/ahadeth_image.png'),
+        Container(
+          color: AppColors.primaryLightColor,
+          height: 3,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'الأحاديث',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+        Container(
+          color: AppColors.primaryLightColor,
+          height: 3,
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (_, index) {
+                return HadethItem(
+                  hadethnum: index,
+                );
+              }),
+        )
+      ],
+    );
+  }
+}
+
+class HadethItem extends StatelessWidget {
+  const HadethItem({super.key, required this.hadethnum});
+  final int hadethnum;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'الحديث رقم ${hadethnum + 1}',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ],
+      ),
     );
   }
 }
