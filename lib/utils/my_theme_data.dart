4959c0207a92826f517dbe680b09b2673f2eeb61
mohammed-sherif34 <mohammedshereef342002@gmail.com>
2024-07-22 14:24:36 +0300

    .

---
+++ lib/utils/my_theme_data.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/utils/app_colors.dart';
+
+class MyThemeData {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primaryLightColor,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.primaryLightColor,
+    ),
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: AppColors.black,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w500,
+        color: AppColors.black,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        color: AppColors.black,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        color: AppColors.white,
+      ),
+    ),
+  );
+}
